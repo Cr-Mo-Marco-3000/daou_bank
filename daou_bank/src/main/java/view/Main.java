d5941bcb1a090cc5034b19c9f53fa75ea86f2bec
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2023-05-02 15:42:50 +0900

    crypt test 추가

--- daou_bank/src/main/java/view/Main.java
+++ daou_bank/src/main/java/view/Main.java
@@ -1,16 +1,13 @@
 package view;
 
-import java.util.Scanner;
 
 import controller.LoginTimer;
-import dto.UserDTO;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
 
-		
 		System.out.println("");
 		System.out.println("\t /       \\  /      \\  /      \\ /  |  /  |       /      \\ /        |/  \\     /  |");
 		System.out.println("\t $$$$$$$  |/$$$$$$  |/$$$$$$  |$$ |  $$ |      /$$$$$$  |$$$$$$$$/ $$  \\   /$$ |");
